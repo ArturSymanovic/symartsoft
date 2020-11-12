c95f9495d0e7d41e677358ee2ec5c71f5749bcf9
Artur Symanovic <artur.symanovic@gmail.com>
2020-11-12 15:17:39 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 az login --identity
-az keyvault secret show --name SampleSecret --vault-name SymartsoftKV --query value
+az keyvault secret show --name SampleSecret --vault-name SymartsoftKV --query value | cat
 service nginx start
 dotnet /app/API.dll
