3c7116a5914df3cb5a1c0dc52f231bd9fdfafd7e
Artur Symanovic <artur.symanovic@gmail.com>
2020-11-13 15:14:08 +0000

    Fix file pathes

--- startup.sh
+++ startup.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 az login --identity
 
-az keyvault secret download --name symartsoftkey --vault-name SymartsoftKV --file symartsoftrawkey.txt
-az keyvault secret download --name beginkey --vault-name SymartsoftKV --file beginkey.txt
-az keyvault secret download --name endkey --vault-name SymartsoftKV --file endkey.txt
+az keyvault secret download --name symartsoftkey --vault-name SymartsoftKV --file /app/symartsoftrawkey.txt
+az keyvault secret download --name beginkey --vault-name SymartsoftKV --file b/app/eginkey.txt
+az keyvault secret download --name endkey --vault-name SymartsoftKV --file /app/endkey.txt
 
-az keyvault secret download --name begin --vault-name SymartsoftKV --file begin.txt
-az keyvault secret download --name end --vault-name SymartsoftKV --file end.txt
-az keyvault secret download --name crt1 --vault-name SymartsoftKV --file crt1.txt
-az keyvault secret download --name crt2 --vault-name SymartsoftKV --file crt2.txt
-az keyvault secret download --name crt3 --vault-name SymartsoftKV --file crt3.txt
+az keyvault secret download --name begin --vault-name SymartsoftKV --file /app/begin.txt
+az keyvault secret download --name end --vault-name SymartsoftKV --file /app/end.txt
+az keyvault secret download --name crt1 --vault-name SymartsoftKV --file /app/crt1.txt
+az keyvault secret download --name crt2 --vault-name SymartsoftKV --file /app/crt2.txt
+az keyvault secret download --name crt3 --vault-name SymartsoftKV --file /app/crt3.txt
 
-cat symartsoftrawkey.txt | tr [:space:] '\n' >> symartsoftkey.txt
-cat crt1.txt | tr [:space:] '\n' >> crt1.crt
-cat crt2.txt | tr [:space:] '\n' >> crt2.crt
-cat crt3.txt | tr [:space:] '\n' >> crt3.crt
+cat /app/symartsoftrawkey.txt | tr [:space:] '\n' >> /app/symartsoftkey.txt
+cat /app/crt1.txt | tr [:space:] '\n' >> /app/crt1.crt
+cat /app/crt2.txt | tr [:space:] '\n' >> /app/crt2.crt
+cat /app/crt3.txt | tr [:space:] '\n' >> /app/crt3.crt
 
-cat begin.txt <(echo) crt1.crt <(echo) end.txt <(echo) begin.txt <(echo) crt2.crt <(echo) end.txt <(echo) begin.txt <(echo) crt3.crt <(echo) end.txt <(echo) >> /etc/ssl/certs/symartsoft.crt
-cat beginkey.txt <(echo) symartsoftkey.txt <(echo) endkey.txt <(echo) >> /etc/ssl/certs/symartsoft.key
+echo "" >> /app/nl
+
+cat /app/begin.txt /app/nl /app/crt1.crt /app/nl /app/end.txt /app/nl /app/begin.txt /app/nl /app/crt2.crt /app/nl /app/end.txt /app/nl /app/begin.txt /app/nl /app/crt3.crt /app/nl /app/end.txt /app/nl >> /etc/ssl/certs/symartsoft.crt
+cat /app/beginkey.txt /app/nl /app/symartsoftkey.txt /app/nl /app/endkey.txt /app/nl >> /etc/ssl/certs/symartsoft.key
 
 cat /etc/ssl/certs/symartsoft.crt
 cat /etc/ssl/certs/symartsoft.key
