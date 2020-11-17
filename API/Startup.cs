610e133e361675ce895b0a99d7d52d661f0303bf
Artur Symanovic <artur.symanovic@gmail.com>
2020-11-17 23:04:49 +0000

    Add DbContext service

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -39,6 +41,10 @@ namespace API
             {
                 connectionString = Configuration.GetConnectionString("symartsoft_prod");
             }
+            services.AddDbContext<DataContext>(options => 
+            {
+                options.UseSqlServer(connectionString);
+            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
