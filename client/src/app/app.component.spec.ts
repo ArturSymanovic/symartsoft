d33d7f7614c9c8a52ca5f789ec3f29cf64d0b1e0
Artur Symanovic <artur.symanovic@gmail.com>
2020-11-23 12:04:59 +0000

    Add toolbar

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,35 +1,50 @@
-import { TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { MatToolbarHarness } from '@angular/material/toolbar/testing'
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 describe('AppComponent', () => {
+
+  let loader: HarnessLoader;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        MatToolbarModule
       ],
       declarations: [
         AppComponent
       ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    loader = TestbedHarnessEnvironment.loader(fixture);
+    fixture.detectChanges();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should create the app component', () => {
+    //const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Symartsoft'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Symartsoft');
+  it(`should render toolbar`, async () => {
+    const toolbar = await loader.getHarness(MatToolbarHarness);
+    console.log((await toolbar.getRowsAsText())[0]);
+    expect (toolbar).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h2').textContent).toContain('Welcome to Symartsoft!');
+  it(`toolbar text shoud contain title`, async () => {
+    const toolbar = await loader.getHarness(MatToolbarHarness);
+    const text = (await toolbar.getRowsAsText())[0];
+    expect (text).toContain(fixture.componentInstance.title);
   });
+
 });
