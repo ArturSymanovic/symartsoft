01d8415acd47959769ef027b63502d1244ec176b
Artur Symanovic <artur.symanovic@gmail.com>
2020-11-24 11:06:30 +0000

    Move navbar to separate component

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -4,10 +4,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { HarnessLoader } from '@angular/cdk/testing';
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
-import { MatToolbarHarness } from '@angular/material/toolbar/testing'
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonHarness } from '@angular/material/button/testing'
-import { MatButtonModule } from '@angular/material/button'
+
 
 describe('AppComponent', () => {
 
@@ -17,8 +14,7 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule,
-        MatToolbarModule
+        RouterTestingModule
       ],
       declarations: [
         AppComponent
@@ -32,26 +28,13 @@ describe('AppComponent', () => {
   });
 
   it('should create the app component', () => {
-    //const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should render toolbar`, async () => {
-    const toolbar = await loader.getHarness(MatToolbarHarness);
-    console.log((await toolbar.getRowsAsText())[0]);
-    expect (toolbar).toBeTruthy();
-  });
-
-  it(`should have title text inside toolbar`, async () => {
-    const toolbar = await loader.getHarness(MatToolbarHarness);
-    const text = (await toolbar.getRowsAsText())[0];
-    expect (text).toContain(fixture.componentInstance.title);
-  });
-
-  it(`should have button`, async () => {
-    const button = await loader.getHarness(MatButtonHarness);
-    expect (button).toBeTruthy();
+  it('should create navbar', () => {
+    const navbar = fixture.nativeElement.querySelector('app-nav');
+    expect (navbar).toBeTruthy();
   });
 
 });
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { NavComponent } from './_sharedcomponents/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
