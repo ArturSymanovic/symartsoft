68f730ac822fb0d1e504a319d14dc7e05b3a7619
Artur Symanovic <artur.symanovic@gmail.com>
2020-12-01 19:31:17 +0000

    Add gtag script and subscribe to router event

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+// declare gtag as a function to access the JS code in TS
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent{
-
-  constructor() {
+export class AppComponent {
+  constructor(public router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'G-GZK6SGX0XG', { page_path: event.urlAfterRedirects });
+        // gtag('event', 'page_view', {
+        //   page_path: event.urlAfterRedirects
+        // })
+      }
+    });
   }
-
 }
