304fb5398bae4710db39bbe86e0b8fe2066d4135
Artur Symanovic <artur.symanovic@gmail.com>
2020-12-16 13:11:53 +0000

    Add controller for testing error handling

---
+++ API/Controllers/ErrorEmittingController.cs
@@ -0,0 +1,48 @@
+using API.Data;
+using API.Models.Auth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class ErrorEmittingController : BaseApiController
+    {
+        public DataContext Context { get; }
+        public ErrorEmittingController(DataContext context)
+        {
+            this.Context = context;
+
+        }
+
+        [Authorize]
+        [HttpGet("unauthorized")]
+        public ActionResult<string> GetUnauthorized()
+        {
+         return "unauthorized data";   
+        }
+
+        [HttpGet("notfound")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var notExistingUser = Context.Users.Find("notexists");
+            
+            if (notExistingUser == null) return NotFound();
+
+            return Ok(notExistingUser);
+        }
+        
+        [HttpGet("servererror")]
+        public ActionResult<string> GetServerError()
+        {
+            var notExistingUser = Context.Users.Find("notexists");
+            var valueThorowsException = notExistingUser.ToString();
+            return valueThorowsException;
+        }
+
+        [HttpGet("badrequest")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("Bad request message");
+        }
+    }
+}
