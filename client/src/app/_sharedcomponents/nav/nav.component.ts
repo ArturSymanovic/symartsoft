c5d4877c5ebc5a96eae8c4e95da3b8203860c00e
Artur Symanovic <artur.symanovic@gmail.com>
2021-01-19 12:46:17 +0000

    Add mobile navbar

--- client/src/app/_sharedcomponents/nav/nav.component.ts
+++ client/src/app/_sharedcomponents/nav/nav.component.ts
@@ -1,20 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/_services/auth.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css'],
 })
-export class NavComponent implements OnInit {
+export class NavComponent implements OnInit, OnDestroy {
   title = 'Symartsoft';
+  subscriptionToLayout: Subscription;
+  showMobileMenu:  boolean = false;
   constructor(
     public authService: AuthService,
     private snackbar: MatSnackBar,
-    private router: Router
-  ) {}
+    private router: Router,
+    private breakPointObserver: BreakpointObserver
+  ) {
+    this.subscriptionToLayout = this.breakPointObserver.observe([
+      Breakpoints.HandsetPortrait
+    ]).subscribe({
+      next: (state) =>{
+        if (state.matches) {
+          this.showMobileMenu = true;
+        } else {
+          this.showMobileMenu = false;
+        }
+      }
+    })
+  }
+  ngOnDestroy(): void {
+    this.subscriptionToLayout.unsubscribe();
+  }
 
   ngOnInit(): void {
   }
